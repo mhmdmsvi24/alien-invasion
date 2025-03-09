8b2e1e935ef27b6727292f64468969fca1148201
mhmdmsvi24 <mousavimohammadg@gmail.com>
2025-03-09 13:29:21 +0330

    fix: ship position don't exceed the screen now

--- ship.py
+++ ship.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Ship:
     def __init__(self, screen):
         self.screen = screen
@@ -11,11 +12,11 @@ class Ship:
         self.screen_rect = screen.get_rect()
 
         # ship position
-        self.rect.centery = self.screen_rect.centery
-        self.rect.left = self.screen_rect.left
+        self.rect.centery = float(self.screen_rect.centery)
+        self.rect.left = float(self.screen_rect.left)
 
         # ship speed
-        self.movement_speed = 5.0
+        self.movement_speed = 0
 
         # movments
         self.moving_right = False
@@ -23,23 +24,23 @@ class Ship:
         self.moving_top = False
         self.moving_bottom = False
 
-
     def update(self):
         """Updates the shop pos based on movement flags for continous movment"""
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.rect.x += self.movement_speed
-        elif self.moving_left:
+        elif self.moving_left and self.rect.left > self.screen_rect.left:
             self.rect.x -= self.movement_speed
-        elif self.moving_top:
+        elif self.moving_top and self.rect.top > self.screen_rect.top:
             self.rect.y -= self.movement_speed
-        elif self.moving_bottom:
+        elif self.moving_bottom and self.rect.bottom < self.screen_rect.bottom:
             self.rect.y += self.movement_speed
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
 
+
 class CargoShip(Ship):
     def __init__(self, screen):
         super().__init__(screen)
+        self.movement_speed = 2.5
         self.image = pygame.image.load("images/space-ship-small.bmp")
-
